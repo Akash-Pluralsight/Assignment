fff1a8e8e197e99de84652bb9663b801a7c9923c
Akash <akashs-chetiar@pluralsight.com>
2024-02-13 15:52:07 +0530

    updated old format of code

--- routes/routes.ts
+++ routes/routes.ts
@@ -6,45 +6,48 @@ import { deleteStudentByID } from '../services/deletesStudentById';
 
 const router = express.Router();
 
-router.post('/addStudent', (req, res) => {
-    const studentData = req.body;
-    if (!studentData || Object.keys(studentData).length === 0) {
-        return res.status(400).send("Invalid student data");
+router.post('/student', async (req, res) => {
+    try {
+        const studentData = req.body;
+        if (!studentData || Object.keys(studentData).length === 0) {
+            return res.status(400).send("Invalid student data");
+        }
+        addStudent(studentData);
+        res.send("Student Added");
+    } catch (error) {
+        res.status(500).send({ error});
     }
-    addStudent(studentData);
-    res.send("Student Added");
 });
 
-router.get('/getStudentDetails',(req,res)=>{
-    getStudent()
-        .then(studentDetails => {
-            res.send(studentDetails);
-        })
-        .catch(error => {
-            res.status(500).send({ error: error.message });
-        });
+router.get('/students', async (req, res) => {
+    try {
+        const studentDetails = await getStudent();
+        res.send(studentDetails);
+    } catch (error) {
+        res.status(500).send({ error });
+    }
 });
 
-router.get('/getStudentById',(req,res)=>{
-    const id:number=req.body.id;
-    getStudentById(id)
-        .then((student: object) => {
-            res.send(student);
-        })
-        .catch((error:Error) => {
-            res.status(500).send({ error: error.message });
-        });
+router.get('/studentbyid/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params['id']);
+        const student = await getStudentById(id);
+        res.send(student);
+    } catch (error) {
+        res.status(500).send({ error });
+    }
 });
 
-router.delete('/deleteStudentByID',(req,res)=>{
-    const id:number=req.body.id;
-    deleteStudentByID(id)
-        .then((updatedStudents: object) => {
-            res.send(updatedStudents);
-        })
-        .catch((error:Error) => {
-            res.status(500).send({ error: error.message });
-        });
+
+router.delete('/deleteStudentByID/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params['id']);
+        const updatedStudents = await deleteStudentByID(id);
+        res.send(updatedStudents);
+    } catch (error) {
+        res.status(500).send({ error });
+    }
 });
+
 
 export default router;
--- server.ts
+++ server.ts
@@ -3,14 +3,14 @@ import myRoutes from './routes/routes';
 const app: Application = express();
 const port = 8000;
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
+app.use('/api', myRoutes);
+
 app.listen(port, () => {
   console.log(`Server is Live at http://localhost:${port}`);
 });
-
-app.use(express.json());
-
-app.use('/api', myRoutes);
